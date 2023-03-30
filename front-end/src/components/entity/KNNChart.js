c95696440978c8dc771e8dba7ec36f75e04aa03a
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2023-03-30 20:12:00 +0100

    Added fully predicted KNN values

--- front-end/src/components/entity/KNNChart.js
+++ front-end/src/components/entity/KNNChart.js
@@ -8,9 +8,14 @@ export const KNNChart = () => {
     const ref3 =  useRef();
     const ref4 =  useRef();
     const [knn, setKNN] = useState();
+    const [knnpredict, setKNNpredict] = useState();
 
     useEffect(() => {
         d3.csv("/datasets/missing/missing-filled-knn.csv", d3.autoType).then(setKNN);
+      }, []);
+
+      useEffect(() => {
+        d3.csv("/datasets/missing/knn-predict-colpg.csv", d3.autoType).then(setKNNpredict);
       }, []);
     
       // Missing Data chart
@@ -45,13 +50,10 @@ export const KNNChart = () => {
         return () => chart2.remove();
       }, [knn])
 
-    useEffect(() => {
-      d3.csv("/datasets/missing/missing-filled-knn.csv", d3.autoType).then(setKNN);
-    }, []);
-  
+
     // Missing Data chart
     useEffect(() => {
-      if(knn === undefined) return;
+      if(knnpredict === undefined) return;
       const chart2 = Plot.plot({
         
         style: {
@@ -72,29 +74,26 @@ export const KNNChart = () => {
         },
         marks: [
           Plot.ruleY([0]),
-          Plot.dot(knn, {x: "carbon-monoxide", y: "lpg",  fill:"smoke"}),
-          Plot.linearRegressionY(knn, {x: "carbon-monoxide", y: "lpg", stroke: "steelblue", ci: 0.95})
+          Plot.dot(knnpredict, {x: "carbon-monoxide", y: "lpg"}),
+          Plot.linearRegressionY(knnpredict, {x: "carbon-monoxide", y: "lpg", stroke: "steelblue", ci: 0.95})
         ],
         marginBottom: 50,
       });
       ref3.current.append(chart2);
       return () => chart2.remove();
-    }, [knn])
+    }, [knnpredict])
 
 return (
     <>
-        <div className="col-lg-6" ref={ref3}>
+        <div className="col-lg-6" ref={ref4}>
     <p>
       Replaced missing values with KNN<br/> File Location:<code>./front-end/src/KNNChart.js</code> and save to reload. 
     </p>
     <p>All missing values are predicted using KNN and plotted to show the difference</p>
     
   </div>
-      <div className="col-lg-6" ref={ref4}>
-      <p>
-        Replaced missing values with KNN<br/> File Location:<code>./front-end/src/KNNChart.js</code> and save to reload. 
-      </p>
-      <p>All missing values are predicted using KNN and plotted to show the difference</p>
+      <div className="col-lg-6" ref={ref3}>
+      <p>All LPG values are predicted using KNN and plotted with CO to show the difference</p>
       
     </div>
     </>
