ad653e5432aa82776c7108c23bd0ab7d1f5c863e
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2023-04-02 02:47:12 +0100

    Fixed Original Data Page
    Added header gif, created card to hold the static image of the chart (to be displayed on the home page), and altered the x and y label values

--- front-end/src/components/entity/OriginalChartGraph.js
+++ front-end/src/components/entity/OriginalChartGraph.js
@@ -2,7 +2,12 @@ import * as Plot from "@observablehq/plot";
 
 import * as d3 from "d3";
 import React, {useEffect, useRef, useState} from 'react';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import { style } from "d3";
 
 
 export function OriginalDataChart(headerRef){
@@ -21,37 +26,71 @@ export function OriginalDataChart(headerRef){
       const chart = Plot.plot({
   
         style: {
-          background: "transparent"
+          background: "white",
+          padding: '20px',
+          color: 'black',
+          marginLeft: "auto",
+          marginBottom: "0",
+          marginRight: "auto",
+          marginTop: "0",
+          textAlign: "center"
         },
         y: {
-          grid: true
+          grid: true,
+          label: "lpg (ppm (%)) ↑"
+        },
+        x: {
+          label: "carbon monoxide (ppm (%)) →"
+        },
+        fill: {
+          textAlign: "center",
+          marginLeft: "auto",
+          marginBottom: "0",
+          marginRight: "auto",
         },
         color: {
           type: "diverging",
-          scheme: "burd",
+          scheme: "buylrd",
           legend: true,
-          background: "transparent",
+          label: "smoke (ppm (%)) →",
+          
           
         },
         marks: [
           Plot.ruleY([0]),
+          
           Plot.dot(bigData, {x: "carbon-monoxide", y: "lpg", fill:"smoke"}),
-          Plot.linearRegressionY(bigData, {x: "carbon-monoxide", y: "lpg", stroke: "steelblue", ci: 0.95})
+          Plot.linearRegressionY(bigData, {x: "carbon-monoxide", y: "lpg", stroke: "steelblue", ci: 0.95}),
+          
         ],
         marginBottom: 50,
+        strokeWidth:0.5
       });
+
   
       headerRef.current.append(chart);
       return () => chart.remove();
     }, [bigData]);
     return (
-    
-        <div className="col" ref={headerRef}>
-        <p>
-          No Missing Values <br/> File Location:<code>./front-end/src/OriginalChartGraph.js</code> and save to reload. 
-        </p>
-        <p>The dataset is reduced by half initally and plotted to identify a relationship. </p>
+      <>
+
+        <div className="col-lg-8" style={{marginLeft:'auto', marginRight:'auto'}}>
+        <div className="card" style={{backgroundColor:"rgba(95, 95, 95, 0.1)", margin:'0, auto'}}>
+        <div className="card-header">
+          header
+          </div>
+        <div className="card-body">
+          <div className="col" ref={headerRef} style={{ marginLeft:'auto', marginRight:'auto'}}>
+          <p>
+            No Missing Values <br/> File Location:<code>./front-end/src/OriginalChartGraph.js</code> and save to reload. 
+          </p>
+          <p>The dataset is reduced by half initally and plotted to identify a relationship. </p>
+          </div>
         </div>
+        </div>
+        </div>
+      </>
+ 
 
     
       )
---
+++ front-end/src/components/ui/Card-Original.js
@@ -0,0 +1,40 @@
+import React from 'react'; 
+import original from '../../assets/img/static/original.PNG'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+export const CardOriginal = () => {
+
+
+return (
+<Card sx={{ maxWidth: 800 }}>
+<CardMedia
+  component="img"
+  alt="Original Data"
+  height="520"
+  image={original}
+/>
+<CardContent>
+  <Typography gutterBottom variant="h5" component="div">
+    Original Data
+  </Typography>
+  <Typography variant="body2" color="text.secondary" style={{fontSize:'16px'}}>
+    The original data is over 400,000 lines long, so for performance this is reduced to 200,000 lines. Exploratory data analysis is performed to identify any relationships between variables. Relationships in data science are where one thing directly affects another, and these two variables often have a high correlation coefficient, which measures how much they depend on eachother.
+  </Typography>
+</CardContent>
+<CardActions >
+<Box textAlign='center'>
+  <a href='/Original'>
+  <Button variant="contained" color='primary' size="large" style={{marginLeft:"auto", marginRight:"auto"}}>View</Button>
+  </a> 
+  </Box>
+</CardActions>
+</Card>
+
+)
+
+}
